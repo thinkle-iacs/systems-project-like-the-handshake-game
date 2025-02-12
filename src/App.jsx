ca6c79dfb9b426cc1567e5bea876d1f049b22c84
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-02-12 21:05:24 +0000

    Begin refactor -- simplify; allow multiple sims; no typescript

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import SimpleSimulation from "./handshakeGame/SimpleSimulation";
+
+export const App = () => {
+  return <SimpleSimulation />;
+};
+
+export default App;
